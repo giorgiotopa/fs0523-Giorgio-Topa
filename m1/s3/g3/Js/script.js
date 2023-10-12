0a890a9367e7d65666e91b206fbb99f1f785c8e8
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-10-12 02:21:54 +0200

    correzione s3/g3

--- m1/s3/g3/Js/script.js
+++ m1/s3/g3/Js/script.js
@@ -2,45 +2,36 @@
 
 let buttonsave = document.getElementById('mybutton')
 
-buttonsave.addEventListener('click', function(e) {
-    
+buttonsave.addEventListener('click', function(e) {    
     e.preventDefault();
     
-
-   let input = document.getElementById('myinput');
-   let list = document.getElementById('mylist');
-
+    let input = document.getElementById('myinput');
+    let list = document.getElementById('mylist');
    if (!input.value) return;
 
    let elementlist = document.createElement('li');
         elementlist.classList.add('listclass');
         elementlist.innerText = input.value;
-
-        list.append(elementlist);
-        input.value = '';  
+        
+        list.prepend(elementlist);
+        input.value = '';         
         
         elementlist.addEventListener('click', function() {
-
             elementlist.classList.add('selectedlist');
             let buttondelete = document.createElement('button');
             buttondelete.innerText = 'Delete';
-            elementlist.appendChild(buttondelete);
-            
-            if (!(buttondelete.value)) {
+
+            elementlist.append(buttondelete);
+        
+            buttondelete.addEventListener('click',function () {
+             elementlist.remove();
                 
-            } else {              
-                
-                buttondelete.addEventListener('click', function (e) {
-                    e.preventDefault();
-                    
-                    elementlist.value=''
-                })
-                
-            }
-            
+            }) 
         })
-           
-})
+ })
+        
+
+
 
 
 
