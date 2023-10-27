20e915cb18f0299ce59570e32d119d6e9dcf37dd
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-10-28 01:09:59 +0200

    Completamento esercizi base progetto m2/s1

---
+++ m2/s1/g5/Js/script.js
@@ -0,0 +1,20 @@
+let nav = document.querySelector('header nav')
+let getstarter = document.querySelector('.getstarted')
+    
+       window.addEventListener("scroll",function(){
+           let top = window.scrollY;
+           if (top >= 320) {
+            //    nav.classList.remove('backdissolvenza')
+               nav.classList.add('nav-bg');
+               getstarter.classList.add('startedbg')
+               
+            } else{
+                nav.classList.remove('nav-bg');
+                getstarter.classList.remove('startedbg')
+                // nav.classList.add('backdissolvenza');
+                
+            } 
+        })
+ 
+    
+ 
