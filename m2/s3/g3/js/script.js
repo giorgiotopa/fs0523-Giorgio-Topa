449780375f25f716177c5b12d16322d277edc696
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-11-08 16:57:00 +0100

    M2-S3-G3 Esercizio

---
+++ m2/s3/g3/js/script.js
@@ -0,0 +1,49 @@
+
+let contentCard = document.getElementById('contentCard')
+
+
+fetch('https://striveschool-api.herokuapp.com/books')
+   .then(response => response.json())
+   .then(res => {
+    res.forEach(el => {
+        
+        let col = document.createElement('div');
+        col.classList.add('col-8', 'col-md-5', 'col-lg-4','col-xxl-3');
+        contentCard.appendChild(col);
+
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.style.width = '18rem';
+        col.appendChild(card);
+
+        let img = document.createElement('img');
+        img.classList.add('card-img-top');       
+        img.src = el.img;
+        img.alt = el.title;
+        img.style.height = '25rem';
+        card.appendChild(img);
+
+        let cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        cardBody.style.height = '13rem';
+        card.appendChild(cardBody);
+
+        let title = document.createElement('h5');
+        title.textContent = el.title;
+        title.classList.add('card-title');
+        cardBody.appendChild(title);
+
+        let price = document.createElement('p');
+        price.textContent = el.price + ' €';
+        cardBody.appendChild(price);
+
+        let button = document.createElement('a');
+        button.classList.add('btn', 'btn-warning');
+        button.innerText = 'Scarta'
+        cardBody.append(button);
+        button.addEventListener('click', () => {
+            card.remove();
+        });
+
+    });
+})
