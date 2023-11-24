31f6ac568e29674854853458b1d55b3e579abe66
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-11-24 13:22:23 +0100

    Completamento Extra M3/S1/G5

--- m3/s1/g5/script.js
+++ m3/s1/g5/script.js
@@ -1,4 +1,6 @@
 "use strict";
+// Interfaccia Sim
+//   Classe Smartphone
 class Smartphone {
     constructor() {
         this.carica = 0;
@@ -9,7 +11,7 @@ class Smartphone {
         this.carica += euro;
     }
     numero404() {
-        return `Credito residuo: ${this.carica} euro`;
+        return `Credito residuo: ${this.carica} €`;
     }
     getNumeroChiamate() {
         return this.numeroChiamate;
@@ -24,3 +26,75 @@ class Smartphone {
         this.numeroChiamate = 0;
     }
 }
+// Istanze Smartphone
+const smartphone1 = new Smartphone();
+const smartphone2 = new Smartphone();
+const smartphone3 = new Smartphone();
+smartphone1.ricarica(30);
+smartphone2.ricarica(50);
+smartphone3.ricarica(20);
+smartphone1.chiamata(10);
+smartphone2.chiamata(5);
+smartphone3.chiamata(20);
+smartphone3.chiamata(30);
+smartphone3.chiamata(30); // Nel console log questa chiamata non apparirà perchè il costo della stessa è superiore al credito residuo
+console.log("Smartphone 1:");
+console.log(smartphone1.numero404());
+console.log(`Numero di chiamate effettuate: ${smartphone1.getNumeroChiamate()}`);
+console.log("\nSmartphone 2:");
+console.log(smartphone2.numero404());
+console.log(`Numero di chiamate effettuate: ${smartphone2.getNumeroChiamate()}`);
+console.log("\nSmartphone 3:");
+console.log(smartphone3.numero404());
+console.log(`Numero di chiamate effettuate: ${smartphone3.getNumeroChiamate()}`);
+smartphone1.azzeraChiamate();
+smartphone2.azzeraChiamate();
+smartphone3.azzeraChiamate();
+console.log("\nDopo l'azzeramento delle chiamate:");
+console.log(`Numero di chiamate smartphone 1: ${smartphone1.getNumeroChiamate()}`);
+console.log(`Numero di chiamate smartphone 2: ${smartphone2.getNumeroChiamate()}`);
+console.log(`Numero di chiamate smartphone 3: ${smartphone3.getNumeroChiamate()}`);
+class Iphone extends Smartphone {
+    constructor() {
+        super(...arguments);
+        this.registroChiamate = [];
+    }
+    chiamata(min) {
+        if (this.carica >= min * this.costoMinuto) {
+            this.carica -= min * this.costoMinuto;
+            this.numeroChiamate++;
+            const nuovaChiamata = {
+                id: this.numeroChiamate,
+                durata: min,
+                dataOra: new Date(),
+            };
+            this.registroChiamate.push(nuovaChiamata);
+        }
+    }
+    mostraRegistroChiamate() {
+        console.log("\nRegistro delle chiamate:");
+        this.registroChiamate.forEach((chiamata) => {
+            console.log(`ID: ${chiamata.id}, Durata: ${chiamata.durata} minuti, Data e Ora: ${chiamata.dataOra}`);
+        });
+    }
+    filtraChiamatePerDataOra(dataEOra) {
+        const chiamateFiltrate = this.registroChiamate.filter((chiamata) => {
+            return chiamata.dataOra.getHours() === dataEOra.getHours(); // il confronto corretto che tenesse conto dell'intera data e non soltanto dell'ora sarebbe sato con il metodo getTime(), ma la precisione del valore era tale da rendere troppo complesso il confronto con una data fortina dall'esterno. 
+        });
+        console.log(`\nChiamate effettuate in data e ora ${dataEOra}:`);
+        chiamateFiltrate.forEach((chiamata) => {
+            console.log(`ID: ${chiamata.id}, Durata: ${chiamata.durata} minuti, Data e Ora: ${chiamata.dataOra}`);
+        });
+    }
+}
+const iphone = new Iphone();
+iphone.ricarica(30);
+iphone.chiamata(5);
+iphone.chiamata(8);
+console.log("\nIphone:");
+console.log(iphone.numero404());
+console.log(`Numero di chiamate effettuate: ${iphone.getNumeroChiamate()}`);
+iphone.mostraRegistroChiamate();
+iphone.filtraChiamatePerDataOra(new Date());
+iphone.azzeraChiamate();
+console.log(`\nNumero di chiamate dopo azzeramento: ${iphone.getNumeroChiamate()}`);
--- m3/s1/g5/script.ts
+++ m3/s1/g5/script.ts
@@ -1,3 +1,5 @@
+// Interfaccia Sim
+
 interface Sim {
     carica: number;
     numeroChiamate: number;
@@ -10,6 +12,7 @@ interface Sim {
     azzeraChiamate(): void;
   }
 
+//   Classe Smartphone
 
   class Smartphone implements Sim {
     carica: number = 0;
@@ -21,7 +24,7 @@ interface Sim {
     }
   
     numero404(): string {
-      return `Credito residuo: ${this.carica} euro`;
+      return `Credito residuo: ${this.carica} €`;
     }
   
     getNumeroChiamate(): number {
@@ -40,4 +43,107 @@ interface Sim {
     }
   }
 
-  
+// Istanze Smartphone
+
+const smartphone1 = new Smartphone();
+const smartphone2 = new Smartphone();
+const smartphone3 = new Smartphone();
+
+smartphone1.ricarica(30);
+smartphone2.ricarica(50);
+smartphone3.ricarica(20);
+
+smartphone1.chiamata(10);
+smartphone2.chiamata(5);
+smartphone3.chiamata(20);
+smartphone3.chiamata(30);
+smartphone3.chiamata(30); // Nel console log questa chiamata non apparirà perchè il costo della stessa è superiore al credito residuo
+
+console.log("Smartphone 1:");
+console.log(smartphone1.numero404());
+console.log(`Numero di chiamate effettuate: ${smartphone1.getNumeroChiamate()}`);
+
+console.log("\nSmartphone 2:");
+console.log(smartphone2.numero404());
+console.log(`Numero di chiamate effettuate: ${smartphone2.getNumeroChiamate()}`);
+
+console.log("\nSmartphone 3:");
+console.log(smartphone3.numero404());
+console.log(`Numero di chiamate effettuate: ${smartphone3.getNumeroChiamate()}`);
+
+
+smartphone1.azzeraChiamate();
+smartphone2.azzeraChiamate();
+smartphone3.azzeraChiamate();
+
+console.log("\nDopo l'azzeramento delle chiamate:");
+console.log(`Numero di chiamate smartphone 1: ${smartphone1.getNumeroChiamate()}`);
+console.log(`Numero di chiamate smartphone 2: ${smartphone2.getNumeroChiamate()}`);
+console.log(`Numero di chiamate smartphone 3: ${smartphone3.getNumeroChiamate()}`);
+
+// EXTRA
+
+interface Chiamata {
+    id: number;
+    durata: number;
+    dataOra: Date;
+  }
+
+class Iphone extends Smartphone{
+
+    registroChiamate: Chiamata[] = [];
+    
+    chiamata(min: number): void {
+        if (this.carica >= min * this.costoMinuto) {
+          this.carica -= min * this.costoMinuto;
+          this.numeroChiamate++;
+    
+          const nuovaChiamata: Chiamata = {
+            id: this.numeroChiamate,
+            durata: min,
+            dataOra: new Date(),
+          };
+          this.registroChiamate.push(nuovaChiamata);
+        }
+      }
+
+      mostraRegistroChiamate(): void {
+        console.log("\nRegistro delle chiamate:");
+        this.registroChiamate.forEach((chiamata) => {
+          console.log(`ID: ${chiamata.id}, Durata: ${chiamata.durata} minuti, Data e Ora: ${chiamata.dataOra}`);
+        });
+      }
+
+      filtraChiamatePerDataOra(dataEOra: Date): void {
+
+        const chiamateFiltrate = this.registroChiamate.filter((chiamata) => {       
+            
+
+        return chiamata.dataOra.getHours() === dataEOra.getHours() // il confronto corretto che tenesse conto dell'intera data e non soltanto dell'ora sarebbe sato con il metodo getTime(), ma la precisione del valore era tale da rendere troppo complesso il confronto con una data fortina dall'esterno. 
+
+        });
+
+        console.log(`\nChiamate effettuate in data e ora ${dataEOra}:`);
+        chiamateFiltrate.forEach((chiamata) => {
+          console.log(`ID: ${chiamata.id}, Durata: ${chiamata.durata} minuti, Data e Ora: ${chiamata.dataOra}`);
+        });
+      }
+    }
+
+const iphone = new Iphone();
+iphone.ricarica(30);
+iphone.chiamata(5);
+iphone.chiamata(8);
+
+console.log("\nIphone:");
+console.log(iphone.numero404());
+console.log(`Numero di chiamate effettuate: ${iphone.getNumeroChiamate()}`);
+
+iphone.mostraRegistroChiamate();
+iphone.filtraChiamatePerDataOra(new Date());
+
+iphone.azzeraChiamate();
+console.log(`\nNumero di chiamate dopo azzeramento: ${iphone.getNumeroChiamate()}`);
+
+
+
