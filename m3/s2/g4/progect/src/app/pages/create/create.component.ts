a008d01a55f1b3929e423ce2522d98573e821f29
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-11-30 22:49:23 +0100

    Bottone Salva

--- m3/s2/g4/progect/src/app/pages/create/create.component.ts
+++ m3/s2/g4/progect/src/app/pages/create/create.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Smartphone } from '../../Models/smartphone';
+import { SmartphoneService } from '../../smartphone.service';
 
 @Component({
   selector: 'app-create',
@@ -8,9 +9,27 @@ import { Smartphone } from '../../Models/smartphone';
 })
 export class CreateComponent {
 
+  constructor(
+    private smartphoneSvc:SmartphoneService,
+    // private router:Router
+    ){}
+
   newSmartphone:Partial<Smartphone> = {
     active: '0'
   };
 
+  oldSmartphone:Smartphone|null = null;
+
+  loading:boolean = false;
+
+  save(){
+    this.loading = true;
+    this.newSmartphone.active = Boolean(Number(this.newSmartphone.active));
+    this.smartphoneSvc.create(this.newSmartphone).then(res => {
+      this.loading = false
+      this.oldSmartphone = res;
+
+    })
+  }
 
 }
