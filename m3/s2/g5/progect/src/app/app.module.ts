c40b3d8af0669c59f159d5e61a8804ea5d3db90f
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2023-12-01 10:29:28 +0100

    eliminzaione componenti aggiuntivi

--- m3/s2/g5/progect/src/app/app.module.ts
+++ m3/s2/g5/progect/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { CompletatiComponent } from './pages/completati/completati.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodosComponent,
+    CompletatiComponent
   ],
   imports: [
     BrowserModule,
--- m3/s2/test/progect/src/app/Models/i-todo.ts
+++
@@ -1,7 +0,0 @@
-export interface ITodo {
-
-  id?:number,
-  title:string,
-  completed:boolean,
-
-}
